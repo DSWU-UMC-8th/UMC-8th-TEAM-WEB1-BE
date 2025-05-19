c8734a7984f4a7658639ea691b304eb68f3863bb
Sojeong Lee <162033491+Sojeong0430@users.noreply.github.com>
2025-05-19 14:30:04 +0900

    ✨ Feat: Swagger 세팅

---
+++ src/main/java/com/example/UMC8th_MiniProject/global/configuration/SwaggerConfig.java
@@ -0,0 +1,26 @@
+package com.example.UMC8th_MiniProject.global.configuration;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI UMC8thAPI() {
+        Info info = new Info()
+                .title("UMC 8th 미니프로젝트")
+                .description("미니프로젝트 PI 명세서")
+                .version("1.0.0");
+
+        return new OpenAPI()
+                .addServersItem(new Server().url("/"))
+                .info(info);
+    }
+}
